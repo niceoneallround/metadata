bdbbc7fa3146d786d80cf8754c6529aaf0b2a920
niceoneallround <niceoneallround@gmail.com>
2016-09-20 14:31:31 -0700

    Add ability to dispatch from md

--- test/testMs.js
+++ test/testMs.js
@@ -3,15 +3,13 @@
 const assert = require('assert');
 const fs = require('fs');
 const jsonldUtils = require('jsonld-utils/lib/jldUtils');
-const PAUtils = require('../lib/privacyAlgorithm').utils;
+const MDUtils = require('../lib/md').utils;
 const PNDataModel = require('data-models/lib/PNDataModel');
-const PN_P = PNDataModel.PROPERTY;
 const PN_T = PNDataModel.TYPE;
-const should = require('should');
 const YAML = require('js-yaml');
 const util = require('util');
 
-describe('test Privacy Algorithm', function () {
+describe('test MS dispatch works', function () {
   'use strict';
 
   let props = { hostname: 'fake.com' };
@@ -20,26 +18,15 @@ describe('test Privacy Algorithm', function () {
     return fs.readFileSync(__dirname + '/data/' + mdFile, 'utf8');
   }
 
-  describe('1 PA tests', function () {
+  describe('1 MS YAML2Node tests', function () {
 
-    it('1.1 should create a PA from a valid PA YAML version', function () {
+    it('1.1 should dispatch to the correct constructor', function () {
       let md = YAML.safeLoad(readFile('PAValid.yml'));
-      let result = PAUtils.YAML2PrivacyAlgorithm(md.privacy_algorithm, props);
-      console.log(result);
-
+      let result = MDUtils.YAML2Metadata(md.privacy_algorithm, props);
       result.should.have.property('@id');
       result.should.have.property('@type');
       assert(jsonldUtils.isType(result, PN_T.Metadata), util.format('PA is not Metadata:%j', result));
       assert(jsonldUtils.isType(result, PN_T.PrivacyAlgorithm), util.format('PA is not Metadata:%j', result));
-      assert(jsonldUtils.isType(result, PN_T.Resource), util.format('PA is not Metadata:%j', result));
-
-      result.should.have.property(PN_P.privacyStep);
-      result[PN_P.privacyStep].length.should.be.equal(1);
-
-      let ps =   result[PN_P.privacyStep][0];
-      ps.should.have.property(PN_P.privacyAction);
-      ps[PN_P.privacyAction].length.should.be.equal(1);
-
     }); // 1.1
   }); // 1
 
