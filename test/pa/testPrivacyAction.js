1663a14adb2cced31b7090a8f9121409a3cd1dbe
niceoneallround <niceoneallround@gmail.com>
2016-10-25 19:08:18 -0700

    Add new PrivacyAction and PrivacyActionInstance

---
+++ test/pa/testPrivacyAction.js
@@ -0,0 +1,60 @@
+/*jslint node: true, vars: true */
+
+const assert = require('assert');
+const fs = require('fs');
+const jsonldUtils = require('jsonld-utils/lib/jldUtils');
+const PActionUtils = require('../../lib/pa/privacyAction').utils;
+const PNDataModel = require('data-models/lib/PNDataModel');
+const PN_P = PNDataModel.PROPERTY;
+const PN_T = PNDataModel.TYPE;
+const should = require('should');
+const YAML = require('js-yaml');
+const util = require('util');
+
+describe('test Privacy Action', function () {
+  'use strict';
+
+  function readFile(mdFile) {
+    return fs.readFileSync(__dirname + '/../data/' + mdFile, 'utf8');
+  }
+
+  describe('1 PAction tests', function () {
+
+    it('1.1 should create a PAaction from a valid PAction YAML version', function () {
+      let md = YAML.safeLoad(readFile('PActionValid.yaml'));
+      let props = { hostname: 'fake.hostname', domainName: 'fake.com', pa: 'fake.pa', privacyStep: 'fake.pstep' };
+      let result = PActionUtils.YAML2Node(md.privacy_action, props);
+
+      result.should.have.property('@id');
+      result.should.have.property('@type');
+      assert(jsonldUtils.isType(result, PN_T.Metadata), util.format('PAction is not Metadata:%j', result));
+      assert(jsonldUtils.isType(result, PN_T.PrivacyAction), util.format('PAction is not PrivacyAction:%j', result));
+      result.should.have.property(PN_P.description);
+
+      result.should.have.property(PN_P.privacyStep);
+      result.should.have.property(PN_P.contentObfuscationAlgorithm);
+      result.should.have.property(PN_P.obfuscationProvider);
+      result.should.have.property(PN_P.obfuscationService);
+      result.should.have.property(PN_P.kms);
+      result.should.have.property(PN_P.skipOrchestration);
+      result.should.have.property(PN_P.schema);
+
+      //
+      // It should be a valid PA if add issuer and creationTime that come from the JWT
+      //
+      let verified = PActionUtils.verify(result, 'fake.hostname', props);
+      assert(!verified, util.format('PAction was not valid?:%j', verified));
+    }); // 1.1
+
+    it('1.2 should nit create a PAaction from an invalid PAction YAML version', function () {
+      let md = YAML.safeLoad(readFile('PActionValid.yaml'));
+      let props = { hostname: 'fake.hostname', domainName: 'fake.com', pa: 'fake.pa', privacyStep: 'fake.pstep' };
+
+      md.privacy_action.kms = null;
+      let result = PActionUtils.YAML2Node(md.privacy_action, props);
+      assert(jsonldUtils.isType(result, PN_T.Error), util.format('PAction is not an Error%j', result));
+
+    }); // 1.2
+  }); // 1
+
+});
---
+++ test/pa/testPrivacyActionInstance.js
@@ -0,0 +1,66 @@
+/*jslint node: true, vars: true */
+
+const assert = require('assert');
+const fs = require('fs');
+const jsonldUtils = require('jsonld-utils/lib/jldUtils');
+const PActionUtils = require('../../lib/pa/privacyAction').utils;
+const PActionIUtils = require('../../lib/pa/privacyActionInstance').utils;
+const PNDataModel = require('data-models/lib/PNDataModel');
+const PN_P = PNDataModel.PROPERTY;
+const PN_T = PNDataModel.TYPE;
+const should = require('should');
+const YAML = require('js-yaml');
+const util = require('util');
+
+describe('test Privacy Action Instance', function () {
+  'use strict';
+
+  function readFile(mdFile) {
+    return fs.readFileSync(__dirname + '/../data/' + mdFile, 'utf8');
+  }
+
+  describe('1 PActionInstance tests', function () {
+
+    it('1.1 should create a PAactionInstance from valid params', function () {
+
+      // create the privacy action
+      let md = YAML.safeLoad(readFile('PActionValid.yaml'));
+      let props = { hostname: 'fake.hostname', domainName: 'fake.com', pa: 'fake.pa', privacyStep: 'fake.pstep' };
+      let paction = PActionUtils.YAML2Node(md.privacy_action, props);
+
+      // blank out fields want to set
+      paction[PN_P.obfuscationService] = null;
+      paction[PN_P.schema] = null;
+
+      // create the privacy pipe, only use id so can be fake
+      let pp = { '@id': 'fake.pp.id' };
+
+      // create the privacy action instance template
+      let pait = {};
+      pait['@id'] = PNDataModel.ids.createPrivacyActionInstanceId('fake.hostname', 'pait_1');
+      pait['@type'] = [PN_T.PrivacyActionInstance];
+      pait[PN_P.privacyAction] = paction['@id'];
+      pait[PN_P.action] = PN_T.Obfuscate;
+      pait[PN_P.obfuscationService] = 'fake.os.id';
+      pait[PN_P.schema] = 'a schema';
+      pait[PN_P.encryptKeyMDJWT] = 'keymdjwt';
+      pait[PN_P.encryptKeyMD] = 'keymd';
+
+      // create the privacy action instance
+      let result = PActionIUtils.create(pait, paction, pp, { hostname: 'fake.hostname', domainName: 'fake.domain.name' });
+      console.log(result);
+      result.should.have.property('@id');
+      result.should.have.property('@type');
+      assert(jsonldUtils.isType(result, PN_T.Metadata), util.format('PAction is not Metadata:%j', result));
+      assert(jsonldUtils.isType(result, PN_T.PrivacyActionInstance), util.format('PAction is not PrivacyActionInstance:%j', result));
+      result.should.have.property(PN_P.privacyAction, paction['@id']);
+      result.should.have.property(PN_P.action, PN_T.Obfuscate);
+      result.should.have.property(PN_P.obfuscationService, 'fake.os.id');
+      result.should.have.property(PN_P.schema, 'a schema');
+      result.should.have.property(PN_P.encryptKeyMD, 'keymd');
+      result.should.have.property(PN_P.encryptKeyMDJWT, 'keymdjwt');
+
+    }); // 1.1
+  }); // 1
+
+});
