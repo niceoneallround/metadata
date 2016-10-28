40b63d69ab1545b550b79963d77a22c070218e0c
niceoneallround <niceoneallround@gmail.com>
2016-10-28 11:56:19 -0700

    Make sure set obfuscation service in action instance

--- test/testPrivacyActionInstance.js
+++ test/testPrivacyActionInstance.js
@@ -75,6 +75,7 @@ describe('test Privacy Action Instance', function () {
       assert(jsonldUtils.isType(result, PN_T.PrivacyActionInstance), util.format('is not %s :%j', PN_T.PrivacyActionInstance, result));
       result.should.have.property(PN_P.privacyAction, 'action-1-id');
       result.should.have.property(PN_P.skipOrchestration, false);
+      result.should.have.property(PN_P.obfuscationService, 'fake.os.id');
       result.should.have.property(PN_P.schema);
       result.should.have.property(PN_P.encryptKeyMDJWT);
 
