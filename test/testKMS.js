b4d00aceeb0da2963c58d7913c9cf23e9a9588d7
niceoneallround <niceoneallround@gmail.com>
2016-11-16 18:31:42 -0800

    Support validation of the KMS provider

--- test/testKMS.js
+++ test/testKMS.js
@@ -4,6 +4,7 @@ const assert = require('assert');
 const fs = require('fs');
 const jsonldUtils = require('jsonld-utils/lib/jldUtils');
 const KMSCanons = require('../lib/KMS').canons;
+const KMSConstants = require('../lib/KMS').CONSTANTS;
 const KMSUtils = require('../lib/KMS').utils;
 const PNDataModel = require('data-models/lib/PNDataModel');
 const PN_P = PNDataModel.PROPERTY;
@@ -34,7 +35,7 @@ describe('KMS Resource Tests', function () {
       assert(jsonldUtils.isType(result, PN_T.KMS), util.format('is not a KMS:%j', result));
       assert(jsonldUtils.isType(result, PN_T.Resource), util.format('is not aResource:%j', result));
 
-      result.should.have.property(PN_P.provider, 'https://kms.test.webshield.io/insecure');
+      result.should.have.property(PN_P.provider, KMSConstants.provider.WEBSHIELD_TEST);
       result.should.have.property(PN_P.algorithm);
       result.should.have.property(PN_P.description, 'test_description');
       result.should.have.property(PN_P.issuer, 'theIssuer');
@@ -43,7 +44,7 @@ describe('KMS Resource Tests', function () {
       result.should.have.property(PN_P.customProps);
       result[PN_P.customProps].should.have.property('http://pn.schema.webshield.io/prop#dummy_prop', 'hello');
 
-      let verified = KMSUtils.verify(result, 'fake.hostname');
+      let verified = KMSUtils.verify(result, props);
       assert(!verified, util.format('was not valid?:%j', verified));
     }); // 1.1
 
