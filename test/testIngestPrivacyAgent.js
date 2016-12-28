8d674cca78f677c0ab269f57157793a46fc6924f
niceoneallround <niceoneallround@gmail.com>
2016-12-28 10:41:22 -0800

    Add default identity syndication algorithm to Ingest Privacy Agent

--- test/testIngestPrivacyAgent.js
+++ test/testIngestPrivacyAgent.js
@@ -41,6 +41,7 @@ describe('IPA Ingest Privacy Agent tests', function () {
       result.should.have.property(PN_P.pnDataModel, 'test_pndatamodel');
       result.should.have.property(PN_P.privacyAlgorithm, 'test_privacy_algorithm');
       result.should.have.property(PN_P.obfuscationService, 'test_os');
+      result.should.have.property(PN_P.identitySyndicationAlgorithm, 'isa-1');
 
       let verified = IPAUtils.verify(result, props);
       assert(!verified, util.format('verify was not valid?:%j', verified));
