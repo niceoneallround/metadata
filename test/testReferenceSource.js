1e36a76dc57f54b15eb29d6e0e7b4978d88f5426
niceoneallround <niceoneallround@gmail.com>
2017-01-03 14:08:54 -0800

    Add privacy algorithm, pn data model, and obfuscation service to reference source

--- test/testReferenceSource.js
+++ test/testReferenceSource.js
@@ -4,6 +4,7 @@ const assert = require('assert');
 const fs = require('fs');
 const jsonldUtils = require('jsonld-utils/lib/jldUtils');
 const RSUtils = require('../lib/referenceSource').utils;
+const RSCanons = require('../lib/referenceSource').canons;
 const PNDataModel = require('data-models/lib/PNDataModel');
 const PN_P = PNDataModel.PROPERTY;
 const PN_T = PNDataModel.TYPE;
@@ -33,15 +34,26 @@ describe('test Reference Source', function () {
       assert(jsonldUtils.isType(result, PN_T.ReferenceSource), util.format('RS is not a ReferenceSource:%j', result));
       assert(jsonldUtils.isType(result, PN_T.Resource), util.format('PA is not Resource:%j', result));
 
-      result.should.have.property(PN_P.provisionPipeURL);
-      result.should.have.property(PN_P.subjectQueryURL);
       result.should.have.property(PN_P.description);
       result.should.have.property(PN_P.issuer, 'theIssuer');
       result.should.have.property(PN_P.creationTime, 'createTime');
 
+      result.should.have.property(PN_P.provisionPipeURL);
+      result.should.have.property(PN_P.subjectQueryURL);
+      result.should.have.property(PN_P.pnDataModel, 'test_pndatamodel');
+      result.should.have.property(PN_P.privacyAlgorithm, 'test_privacy_algorithm');
+      result.should.have.property(PN_P.obfuscationService, 'test_os');
+
       let verified = RSUtils.verify(result, 'fake.hostname');
       assert(!verified, util.format('RS was not valid?:%j', verified));
     }); // 1.1
+
+    it('1.3 canon should be valid', function () {
+      let props = { hostname: 'fake.hostname', domainName: 'fake.com', issuer: 'theIssuer', creationTime: 'createTime' };
+      let result = RSCanons.createTestReferenceSource(props);
+      let verified = RSUtils.verify(result, props);
+      assert(!verified, util.format('was not valid?:%j', verified));
+    }); // 1.2
   }); // 1
 
 });
